dbcec1fb076c5a02e7917c8bd7b0dad3c62c1839
poppycars22 <lukejace55@gmail.com>
2022-02-17 20:00:46 -0600

    (:<
    (:<

---
+++ ChaosPoppycarsCards/ChaosPoppycarsCards/Cards/PoppysChaos.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+using BepInEx;
+using ChaosPoppycarsCards.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+using ChaosPoppycarsCards.MonoBehaviours;
+
+namespace ChaosPoppycarsCards.Cards
+{
+    class PoppysChaos : CustomCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            statModifiers.movementSpeed = 2f;
+            gun.reloadTime = 0.5f;
+            gun.ammo = 20;
+            gun.attackSpeed = 0.75f;
+            statModifiers.health = 0.5f;
+            UnityEngine.Debug.Log($"[{ChaosPoppycarsCards.ModInitials}][Card] {GetTitle()} has been setup.");
+            //Edits values on card itself, which are then applied to the player in `ApplyCardStats`
+        }
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            var mono = player.gameObject.GetOrAddComponent<SpeedEffect>();
+            UnityEngine.Debug.Log($"[{ChaosPoppycarsCards.ModInitials}][Card] {GetTitle()} has been added to player {player.playerID}.");
+            //Edits values on player when card is selected
+        }
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            UnityEngine.Debug.Log($"[{ChaosPoppycarsCards.ModInitials}][Card] {GetTitle()} has been removed from player {player.playerID}.");
+            //Run when the card is removed from the player
+        }
+
+        protected override string GetTitle()
+        {
+            return "Poppys Chaos";
+        }
+        protected override string GetDescription()
+        {
+            return "This cards effects change every update";
+        }
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Uncommon;
+        }
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                
+            };
+        }
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.ColdBlue;
+        }
+        public override string GetModName()
+        {
+            return "CPC";
+        }
+    }
+}
