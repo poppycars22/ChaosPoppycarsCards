90f4652bff6aea75a1a037bf3f8ee200912b5462
SchoolPoppycars <105066546+SchoolPoppycars@users.noreply.github.com>
2022-05-06 11:32:10 -0400

    Create MountainDewMONO.cs

---
+++ ChaosPoppycarsCards/ChaosPoppycarsCards/MonoBehaviours/MountainDewMONO.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using ModdingUtils.MonoBehaviours;
+using UnboundLib.Cards;
+using UnboundLib;
+using ModdingUtils.Extensions;
+using System;
+using System.Collections.Generic;
+using ChaosPoppycarsCards.Cards;
+
+namespace ChaosPoppycarsCards.MonoBehaviours
+{
+    internal class MTDSodaEffect : ReversibleEffect
+    {
+        private float duration = 0;
+        public override void OnOnDestroy()
+        {
+            block.BlockAction = (Action<BlockTrigger.BlockTriggerType>)Delegate.Remove(block.BlockAction, new Action<BlockTrigger.BlockTriggerType>(OnBlock));
+        }
+        private void OnBlock(BlockTrigger.BlockTriggerType trigger)
+        {
+            if (duration <= 0)
+            {
+                ApplyModifiers();
+            }
+            duration = 5f;
+             ColorEffect effect = player.gameObject.AddComponent<ColorEffect>();
+            effect.SetColor(Color.green);
+        }
+
+        public override void OnStart()
+        {
+            characterStatModifiersModifier.lifeSteal_add = 2.5f; //make it affect speed, jumpheight, and increase size
+            characterStatModifiersModifier.sizeMultiplier_mult = 0.5f;
+            blockModifier.cdMultiplier_mult = 1.5f;
+            block.BlockAction = (Action<BlockTrigger.BlockTriggerType>)Delegate.Combine(block.BlockAction, new Action<BlockTrigger.BlockTriggerType>(OnBlock));
+            SetLivesToEffect(int.MaxValue);
+        }
+        public override void OnUpdate()
+        {
+            if (!(duration <= 0))
+            {
+                duration -= TimeHandler.deltaTime;
+            }
+            else
+            {
+                ClearModifiers();
+                UnityEngine.GameObject.Destroy(this.gameObject.GetOrAddComponent<ColorEffect>());
+            }
+        }
+        public override void OnOnDisable()
+        {
+            duration = 0;
+        }
+    }
+}
