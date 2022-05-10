fa11009fc5f1dbe8aafa4ac22aa8b0c458eaa373
SchoolPoppycars <105066546+SchoolPoppycars@users.noreply.github.com>
2022-05-10 11:32:47 -0400

    Update LightSaber.cs

--- ChaosPoppycarsCards/ChaosPoppycarsCards/Cards/Normal/LightSaber.cs
+++ ChaosPoppycarsCards/ChaosPoppycarsCards/Cards/Normal/LightSaber.cs
@@ -1,1 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+using BepInEx;
+using ChaosPoppycarsCards.Cards;
+using ChaosPoppycarsCards.Utilities;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
 
+namespace ChaosPoppycarsCards.Cards
+{
+    class LightSaber : CustomCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            CPCDebug.Log($"[{ChaosPoppycarsCards.ModInitials}][Card] {GetTitle()} has been setup.");
+            //Edits values on card itself, which are then applied to the player in `ApplyCardStats`
+            //make this card give x-1 attack speed, near instant reload, +100% damage, x 0 bounces, no gravity and if you can figure out how, a light saber png attached to the gun that also makes the bullets invisable and makes the gun auto fire
+        }
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            CPCDebug.Log($"[{ChaosPoppycarsCards.ModInitials}][Card] {GetTitle()} has been added to player {player.playerID}.");
+            //Edits values on player when card is selected
+        }
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            CPCDebug.Log($"[{ChaosPoppycarsCards.ModInitials}][Card] {GetTitle()} has been removed from player {player.playerID}.");
+            //Run when the card is removed from the player
+        }
+
+        protected override string GetTitle()
+        {
+            return "Light Saber";
+        }
+        protected override string GetDescription()
+        {
+            return "Gain a light saber";
+        }
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Rare;
+        }
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Attack Speed",
+                    amount = "Almost Instant",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+                new CardInfoStat()
+                {
+                    positive = false,
+                    stat = "Range",
+                    amount = "Very Little",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Damage",
+                    amount = "+100%",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Reload",
+                    amount = "Almost Instant",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+                new CardInfoStat()
+                {
+                    positive = false,
+                    stat = "Bounces",
+                    amount = "None",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Bullet gravity",
+                    amount = "None",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                }
+            };
+        }
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.ColdBlue;
+        }
+        public override string GetModName()
+        {
+            return "CPC";
+        }
+    }
+}
