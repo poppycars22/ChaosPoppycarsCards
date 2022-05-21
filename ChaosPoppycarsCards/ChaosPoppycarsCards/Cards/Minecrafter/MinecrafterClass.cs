88ffd6ca37529cd7d578a2f5f2f84b5117328bb7
poppycars22 <lukejace55@gmail.com>
2022-05-20 19:20:47 -0600

    BALANCE

--- ChaosPoppycarsCards/ChaosPoppycarsCards/Cards/Minecrafter/MinecrafterClass.cs
+++ ChaosPoppycarsCards/ChaosPoppycarsCards/Cards/Minecrafter/MinecrafterClass.cs
@@ -18,25 +18,25 @@ namespace ChaosPoppycarsCards.Cards.Minecrafter
             while (!(CraftingTable.Card && MCBow.Card && MCShield.Card && WoodenSword.Card && StoneSword.Card && IronSword.Card && GoldSword.Card && DiamondSword.Card && NetheriteSword.Card && WoodenAxe.Card && StoneAxe.Card && IronAxe.Card && GoldAxe.Card && DiamondAxe.Card && NetheriteAxe.Card && AttackSpeed.Card && StoneHoe.Card && IronHoe.Card && GoldHoe.Card && DiamondHoe.Card && NetheriteHoe.Card && Larmor.Card && Carmor.Card && Iarmor.Card && Garmor.Card && Darmor.Card && Narmor.Card && BrewingStand.Card && InvisPotion.Card && JumpPotion.Card && RegenPotion.Card && SpeedPotion.Card && StrengthPotion.Card && TotemOfUndying.Card && UltimatePotion.Card)) yield return null;
             ClassesRegistry.Register(CraftingTable.Card, CardType.Entry);
             ClassesRegistry.Register(MCShield.Card, CardType.Card, CraftingTable.Card);
-            ClassesRegistry.Register(WoodenSword.Card, CardType.SubClass, CraftingTable.Card);
+            ClassesRegistry.Register(WoodenSword.Card, CardType.Gate, CraftingTable.Card);
                 ClassesRegistry.Register(StoneSword.Card, CardType.Gate, WoodenSword.Card);
                     ClassesRegistry.Register(IronSword.Card, CardType.Gate, StoneSword.Card);
             ClassesRegistry.Register(GoldSword.Card, CardType.Card, CraftingTable.Card);
                         ClassesRegistry.Register(DiamondSword.Card, CardType.Gate, IronSword.Card);
                             ClassesRegistry.Register(NetheriteSword.Card, CardType.Card, DiamondSword.Card);
-            ClassesRegistry.Register(WoodenAxe.Card, CardType.SubClass, CraftingTable.Card);
+            ClassesRegistry.Register(WoodenAxe.Card, CardType.Gate, CraftingTable.Card);
                 ClassesRegistry.Register(StoneAxe.Card, CardType.Gate, WoodenAxe.Card);
                     ClassesRegistry.Register(IronAxe.Card, CardType.Gate, StoneAxe.Card);
             ClassesRegistry.Register(GoldAxe.Card, CardType.Card, CraftingTable.Card);
                         ClassesRegistry.Register(DiamondAxe.Card, CardType.Gate, IronAxe.Card);
                             ClassesRegistry.Register(NetheriteAxe.Card, CardType.Card, DiamondAxe.Card);
-            ClassesRegistry.Register(AttackSpeed.Card, CardType.SubClass, CraftingTable.Card);
+            ClassesRegistry.Register(AttackSpeed.Card, CardType.Gate, CraftingTable.Card);
                 ClassesRegistry.Register(StoneHoe.Card, CardType.Gate, AttackSpeed.Card);
                     ClassesRegistry.Register(IronHoe.Card, CardType.Gate, StoneHoe.Card);
             ClassesRegistry.Register(GoldHoe.Card, CardType.Card, CraftingTable.Card);
                         ClassesRegistry.Register(DiamondHoe.Card, CardType.Gate, IronHoe.Card);
                             ClassesRegistry.Register(NetheriteHoe.Card, CardType.Card, DiamondHoe.Card);
-            ClassesRegistry.Register(Larmor.Card, CardType.SubClass, CraftingTable.Card);
+            ClassesRegistry.Register(Larmor.Card, CardType.Gate, CraftingTable.Card);
                 ClassesRegistry.Register(Carmor.Card, CardType.Gate, Larmor.Card);
                     ClassesRegistry.Register(Iarmor.Card, CardType.Gate, Carmor.Card);
             ClassesRegistry.Register(Garmor.Card, CardType.Card, CraftingTable.Card);
@@ -69,6 +69,22 @@ namespace ChaosPoppycarsCards.Cards.Minecrafter
                 //Obsidian
                 //(tier system)
             //Farmer
+        }
+        public override IEnumerator PostInit()
+        {
+            ClassesRegistry.Get(WoodenSword.Card).Blacklist(WoodenAxe.Card);
+            ClassesRegistry.Get(WoodenSword.Card).Blacklist(AttackSpeed.Card);
+            ClassesRegistry.Get(WoodenSword.Card).Blacklist(Larmor.Card);
+            ClassesRegistry.Get(WoodenAxe.Card).Blacklist(WoodenSword.Card);
+            ClassesRegistry.Get(WoodenAxe.Card).Blacklist(AttackSpeed.Card);
+            ClassesRegistry.Get(WoodenAxe.Card).Blacklist(Larmor.Card);
+            ClassesRegistry.Get(AttackSpeed.Card).Blacklist(WoodenSword.Card);
+            ClassesRegistry.Get(AttackSpeed.Card).Blacklist(WoodenAxe.Card);
+            ClassesRegistry.Get(AttackSpeed.Card).Blacklist(Larmor.Card);
+            ClassesRegistry.Get(Larmor.Card).Blacklist(WoodenSword.Card);
+            ClassesRegistry.Get(Larmor.Card).Blacklist(AttackSpeed.Card);
+            ClassesRegistry.Get(Larmor.Card).Blacklist(WoodenAxe.Card);
+            yield break;
         }
     }
 }
--- ChaosPoppycarsCards/ChaosPoppycarsCards/ChaosPoppycarsCards.cs
+++ ChaosPoppycarsCards/ChaosPoppycarsCards/ChaosPoppycarsCards.cs
@@ -32,7 +32,7 @@ namespace ChaosPoppycarsCards
     {
         private const string ModId = "com.Poppycars.CPC.Id";
         private const string ModName = "ChaosPoppycarsCards";
-        public const string Version = "0.5.3"; // What version are we on (major.minor.patch)?
+        public const string Version = "0.5.4"; // What version are we on (major.minor.patch)?
         public const string ModInitials = "CPC";
         public static ChaosPoppycarsCards Instance { get; private set; }
         public static object CPC_Assets { get; internal set; }
