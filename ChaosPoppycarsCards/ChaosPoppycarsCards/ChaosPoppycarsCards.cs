990189acc259ddc6e8e4b412d4e4d0cbc94dc0f4
poppycars22 <lukejace55@gmail.com>
2022-06-23 20:03:08 -0600

    properly builds the 2 new cards

--- ChaosPoppycarsCards/ChaosPoppycarsCards/ChaosPoppycarsCards.cs
+++ ChaosPoppycarsCards/ChaosPoppycarsCards/ChaosPoppycarsCards.cs
@@ -34,7 +34,7 @@ namespace ChaosPoppycarsCards
     {
         private const string ModId = "com.Poppycars.CPC.Id";
         private const string ModName = "ChaosPoppycarsCards";
-        public const string Version = "0.5.7"; // What version are we on (major.minor.patch)?
+        public const string Version = "0.5.8"; // What version are we on (major.minor.patch)?
         public const string ModInitials = "CPC";
         public static ChaosPoppycarsCards Instance { get; private set; }
         public static object CPC_Assets { get; internal set; }
@@ -112,6 +112,8 @@ namespace ChaosPoppycarsCards
             CustomCard.BuildCard<ExplosiveArrows>((card) => ExplosiveArrows.Card = card);
             CustomCard.BuildCard<BouncyArrows>((card) => BouncyArrows.Card = card);
             CustomCard.BuildCard<Arrows>((card) => Arrows.Card = card);
+            CustomCard.BuildCard<Punch2>((card) => Punch2.Card = card);
+            CustomCard.BuildCard<SpeedCurse>();
             // CustomCard.BuildCard<WoodenPickaxe>();
             // CustomCard.BuildCard<StonePickaxe>();
             // CustomCard.BuildCard<GoldPickaxe>();
