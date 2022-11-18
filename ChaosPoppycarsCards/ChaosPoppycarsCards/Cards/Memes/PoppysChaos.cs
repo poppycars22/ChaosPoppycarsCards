8372624cc81460db83e50118cfdc46fd62771a82
poppycars22 <92829663+poppycars22@users.noreply.github.com>
2022-11-17 22:16:40 -0500

    art brrrr

--- ChaosPoppycarsCards/ChaosPoppycarsCards/Cards/Memes/PoppysChaos.cs
+++ ChaosPoppycarsCards/ChaosPoppycarsCards/Cards/Memes/PoppysChaos.cs
@@ -27,16 +27,16 @@ namespace ChaosPoppycarsCards.Cards
 
         public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
         {
-            gun.projectielSimulatonSpeed = 0.5f;
-            gun.projectileSize = 1.5f;
-            gun.drag = 1.2f;
-            gun.gravity = 0.75f;
+            gun.projectileSize = 0.001f;
+            gun.damage = 2.5f;
+            gun.reloadTime = 0.5f;
+            
             CPCDebug.Log($"[{ChaosPoppycarsCards.ModInitials}][Card] {GetTitle()} has been setup.");
             //Edits values on card itself, which are then applied to the player in `ApplyCardStats`
         }
         public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
         {
-            
+            gun.spread *= 0f;
             CPCDebug.Log($"[{ChaosPoppycarsCards.ModInitials}][Card] {GetTitle()} has been added to player {player.playerID}.");
             //Edits values on player when card is selected
 
